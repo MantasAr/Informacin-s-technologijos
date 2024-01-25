3edb3a64e673d7e3c50e7934ff09067274564f06
MantasAr <150686005+MantasAr@users.noreply.github.com>
2024-01-25 13:52:00 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // Įtraukiame iostream biblioteką, kad galėtume naudoti įvesties ir išvesties operacijas.
+
+using namespace std;  // Kad nereikėtų rašyti std prie kiekvienos funkcijos
+
+
+// Funkcija, kuri generuoja ir išveda sakinį pagal pateiktus duomenis
+void generuotiSakini(string vardas, string pavarde, int amzius, string profesija) {
+    // Išvedamas sakinys su duomenimis
+    cout << vardas << " " << pavarde << " yra " << amzius << "-aisiais metais gimes " << profesija << "." << endl;
+}
+
+int main() {
+    // Pirmasis kvietimas funkcijai su konkrečiais duomenimis
+    generuotiSakini("Jonas", "Jonaitis", 30, "programuotojas");
+
+    // Antrasis kvietimas funkcijai su skirtingais duomenimis
+    generuotiSakini("Marius", "Mariukas", 25, "inzinierius");
+
+    return 0;
+}
